7b94e1fa5daf8ab57d425d99e2b9e18eb072c3d2
Veris Juniardi <veris.juniardi@gmail.com>
2017-11-19 06:49:30 +0700

    database change

---
+++ application/logs/log-2017-11-19.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-11-19 06:13:13 --> 404 Page Not Found: Assets/theme
+ERROR - 2017-11-19 06:13:16 --> 404 Page Not Found: Assets/theme
