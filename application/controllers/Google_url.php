43cc2ee1f7a9712b4cd2e071bf85750f28acb767
Veris Juniardi <veris.juniardi@gmail.com>
2017-11-24 02:02:30 +0700

    adding new

--- application/controllers/Google_url.php
+++ application/controllers/Google_url.php
@@ -10,7 +10,7 @@ class Google_url extends CI_Controller {
 
     function index()
     {
-        $url = '/validate/view/';
+        $url = 'validate/view/';
         /* if you want switch debug mode, please replace FALSE with TRUE*/
         $this->google_url_api->enable_debug(FALSE);
 
---
+++ application/logs/log-2017-11-24.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-11-24 02:01:26 --> 404 Page Not Found: Validate/view
