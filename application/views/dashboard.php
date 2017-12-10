dcbed57aa877451abff7a8f3654ea30cd2813220
Veris Juniardi <veris.juniardi@gmail.com>
2017-12-10 23:30:54 +0700

    fix setting disabled

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -49,25 +49,25 @@
           <div class="scanner-laser laser-leftTop" style="opacity: 0.5;"></div>
        </div>
 
-       <div class="well" style="width: 100%;">
-                            <label id="zoom-value" width="100">Zoom: 2</label>
-                            <input id="zoom" onchange="Page.changeZoom();" type="range" min="10" max="30" value="20">
-                            <label id="brightness-value" width="100">Brightness: 0</label>
-                            <input id="brightness" onchange="Page.changeBrightness();" type="range" min="0" max="128" value="0">
-                            <label id="contrast-value" width="100">Contrast: 0</label>
-                            <input id="contrast" onchange="Page.changeContrast();" type="range" min="0" max="64" value="0">
-                            <label id="threshold-value" width="100">Threshold: 0</label>
-                            <input id="threshold" onchange="Page.changeThreshold();" type="range" min="0" max="512" value="0">
-                            <label id="sharpness-value" width="100">Sharpness: off</label>
-                            <input id="sharpness" onchange="Page.changeSharpness();" type="checkbox">
-                            <label id="grayscale-value" width="100">grayscale: off</label>
-                            <input id="grayscale" onchange="Page.changeGrayscale();" type="checkbox">
-                            <br>
-                            <label id="flipVertical-value" width="100">Flip Vertical: off</label>
-                            <input id="flipVertical" onchange="Page.changeVertical();" type="checkbox">
-                            <label id="flipHorizontal-value" width="100">Flip Horizontal: off</label>
-                            <input id="flipHorizontal" onchange="Page.changeHorizontal();" type="checkbox">
-                        </div>
+       <!-- <div class="well" style="width: 100%;">
+                <label id="zoom-value" width="100">Zoom: 2</label>
+                <input id="zoom" onchange="Page.changeZoom();" type="range" min="10" max="30" value="20">
+                <label id="brightness-value" width="100">Brightness: 0</label>
+                <input id="brightness" onchange="Page.changeBrightness();" type="range" min="0" max="128" value="0">
+                <label id="contrast-value" width="100">Contrast: 0</label>
+                <input id="contrast" onchange="Page.changeContrast();" type="range" min="0" max="64" value="0">
+                <label id="threshold-value" width="100">Threshold: 0</label>
+                <input id="threshold" onchange="Page.changeThreshold();" type="range" min="0" max="512" value="0">
+                <label id="sharpness-value" width="100">Sharpness: off</label>
+                <input id="sharpness" onchange="Page.changeSharpness();" type="checkbox">
+                <label id="grayscale-value" width="100">grayscale: off</label>
+                <input id="grayscale" onchange="Page.changeGrayscale();" type="checkbox">
+                <br>
+                <label id="flipVertical-value" width="100">Flip Vertical: off</label>
+                <input id="flipVertical" onchange="Page.changeVertical();" type="checkbox">
+                <label id="flipHorizontal-value" width="100">Flip Horizontal: off</label>
+                <input id="flipHorizontal" onchange="Page.changeHorizontal();" type="checkbox">
+            </div> -->
       </div>
       <div class="col-md-6">
       <div class="thumbnail" id="result">
