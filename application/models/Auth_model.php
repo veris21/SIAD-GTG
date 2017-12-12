d79de3a4f514e42738ee204dc2fe10a3fe78a520
Veris Juniardi <veris.juniardi@gmail.com>
2017-12-12 22:34:38 +0700

    fix auth

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -10,16 +10,12 @@ class Auth_model extends CI_Model{
   }
 
   public function auth($uid, $pass){
-      $query = "SELECT u.id as id,
-       u.fullname as fullname, 
-       u.hp as hp, 
-       u.time as last_login,
-       u.avatar as avatar,
-       u.desa_id as desa_id,
-       j.jabatan as jabatan
-       FROM users as u, jabatan as j
-       WHERE u.jabatan_id = j.id AND u.uid = '".$uid."' AND u.pass = '".$pass."'";
-      return $this->db->query($query);      
+      $this->db->select('u.*, j.jabatan, d.nama_desa');
+      $this->db->from('users u, jabatan j, desa d');
+      $this->db->where('u.uid', $uid);
+      $this->db->where('u.pass', $pass);
+      $this->db->where('u.jabatan_id=j.id');
+      return $this->db->get();
   }
 
 }
