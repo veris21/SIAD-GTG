1e1acff8e7c1e99566b65121ea916ac73747f60b
Veris Juniardi <veris.juniardi@gmail.com>
2018-01-07 21:54:45 +0700

    up

--- application/views/pertanahan_layout/print/denah_situasi.php
+++ application/views/pertanahan_layout/print/denah_situasi.php
@@ -18,14 +18,14 @@
                 <td colspan="2">LAMPIRAN I</td>
             </tr>
             <tr>
-                <td width="30%"><b style="font-size:11px;">Nomor </b></td>
-                <td width="70%"> : 
+                <td width="20%"><b style="font-size:11px;">Nomor </b></td>
+                <td width="80%"> : 
                 <b style="font-size:11px;font-family: Consolas, Monaco, Courier New, Courier, monospace;"><?php echo "181/".$data['id']."-".$type."/KTD.".strtoupper($data['nama_desa'])."/".romawi(mdate("%m",$data['time']))."/".mdate("%Y",$data['time']);?></b>
                 </td>
             </tr>
             <tr>
-                <td width="30%"><b style="font-size:11px;">Tanggal </b></td>
-                <td width="70%">: 
+                <td width="20%"><b style="font-size:11px;">Tanggal </b></td>
+                <td width="80%">: 
                     <b  style="font-size:11px;">
                 <?php echo mdate("%d",$data['time'])." ".bulan(mdate("%m",$data['time']))." ".mdate("%Y",$data['time']);?></b>
                 </td>
@@ -79,12 +79,12 @@
         <tr>
             <td valign="middle" width="10%">
                <center>
-                <img width="40" src="<?php echo base_url().'assets/n-1.png'; ?>" alt="NORTH">
+                <img width="40" src="<?php echo base_url().'assets/n-1.png'; ?>" >
                </center>
             </td>
             <td valign="middle" colspan="3" width="90%">
                 <center>
-                <img src="<?php echo base_url().POLYGON.$data['peta']; ?>" alt="CANVAS PETA">
+                <img width="100%" src="<?php echo base_url().POLYGON.$data['peta']; ?>" >
                </center>
             </td>
         </tr>
