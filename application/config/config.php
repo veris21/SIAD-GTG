f7cbc7ffa839180a270c7f3f7a348434bf63c72d
Veris Juniardi <veris.juniardi@gmail.com>
2018-01-17 14:09:05 +0700

    up

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+
 $config['base_url'] = 'https://si-desa.id/';
 // $config['base_url'] = 'http://localhost/SIA-GTG.github/';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
